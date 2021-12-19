f696b6b90b720f984b74ddb9ac2694b49d745cb6
Pedro Henrique <89044375+PedroHSJ@users.noreply.github.com>
2021-12-19 12:19:04 -0300

    Add files via upload

---
+++ HeaderMovie.js
@@ -0,0 +1,90 @@
+const Api_Base = 'https://api.themoviedb.org/3';
+const API_Key = 'fa8367b24166839e18a329a3a990a6a3';
+
+const Genero_recomendado = `/trending/tv/week?language=pt-BR&api_key=${API_Key}`;
+
+const header = document.getElementsByTagName("header")[0];
+const filtro = document.getElementById('filtro');
+
+const h1 = document.getElementById('titulo-header');
+const info = document.getElementById('info');
+const details = document.getElementById('details');
+const pontuacao = document.getElementById('pontuacao');
+const year = document.getElementById('year');
+
+const divbtn = document.createElement('div');
+divbtn.id = 'buttons';
+
+const Btnassistir = document.createElement('button');
+Btnassistir.id = 'assistir';
+Btnassistir.innerText = 'Assistir';
+
+const Btnlista = document.createElement('button');
+Btnlista.id = 'lista';
+Btnlista.innerText = '+ Minha lista';
+
+divbtn.appendChild(Btnassistir);
+divbtn.appendChild(Btnlista);
+
+
+function Random(max) {
+    return i = Math.floor(Math.random() * max)
+}
+
+
+function InfoHeader(chosen) {
+
+    h1.innerHTML = chosen.name;
+    info.innerHTML = chosen.overview;
+    pontuacao.innerHTML = chosen.vote_average + ' pontos';
+    year.innerHTML = chosen.first_air_date.split('-')[0];
+
+
+
+
+    filtro.append(h1)
+    filtro.append(details)
+    filtro.append(info)
+    filtro.appendChild(divbtn);
+    details.append(pontuacao)
+    details.append(year)
+};
+
+window.onload = setInterval(() => {
+    axios.get(`${Api_Base}${Genero_recomendado}`)
+        .then(response => {
+            const i = Random(response.data.results.length - 1);
+
+            const chosen = response.data.results[i];
+
+            header.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${chosen.backdrop_path})`;
+            console.log(chosen)
+            InfoHeader(chosen)
+        })
+}, 300000)
+
+axios.get(`${Api_Base}${Genero_recomendado}`)
+    .then(response => {
+        const i = Random(response.data.results.length - 1);
+
+        const chosen = response.data.results[i];
+
+        header.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${chosen.backdrop_path})`;
+        console.log(chosen)
+        InfoHeader(chosen)
+    });
+
+const nav = document.getElementsByTagName('nav')[0];
+
+
+
+window.addEventListener('scroll', () => {
+        if(scrollY >= 10){
+            nav.classList.add('black')
+        }else if(scrollY === 0){
+            nav.classList.remove('black')
+        }
+        console.log(scrollY)
+});
+
+
---
+++ Movie-Row.js
@@ -0,0 +1,327 @@
+const API_KEY = 'fa8367b24166839e18a329a3a990a6a3';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+const genero_originals = `/discover/tv?with_network=123&language=pt-BR&api_key=${API_KEY}`
+const genero_recomendado = `/trending/all/week?language=pt-BR&api_key=${API_KEY}`
+const genero_EmAlta = `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`
+const genero_acao = `/discover/movie?with_genres=28&languge=pt-BR&api_key=${API_KEY}`
+const genero_animacao = `/discover/movie?with_genres=16&languge=pt-BR&api_key=${API_KEY}`
+const genero_terror = `/discover/movie?with_genres=27&languge=pt-BR&api_key=${API_KEY}`
+const genero_romance = `/discover/movie?with_genres=10749&languge=pt-BR&api_key=${API_KEY}`
+const genero_documentario = `/discover/movie?with_genres=99&languge=pt-BR&api_key=${API_KEY}`
+
+const originals = document.getElementById('Originals');
+const recomendado = document.getElementById('Recomendado');
+const emAlta = document.getElementById('EmAlta');
+const acao = document.getElementById('Acao');
+const animacao = document.getElementById('Animacao');
+const terror = document.getElementById('Terror');
+const romance = document.getElementById('Romance');
+const documentario = document.getElementById('Documentario');
+
+
+const div = document.getElementsByClassName('movie-row');
+
+
+
+axios.get(`${API_BASE}${genero_originals}`)
+    .then(response => {
+        const filmes = response.data.results.map((filme, i) => {
+
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        originals.innerHTML += filmes;
+
+    })
+
+axios.get(`${API_BASE}${genero_recomendado}`)
+    .then(response => {
+        const filmes = response.data.results.map(filme => {
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        recomendado.innerHTML += filmes
+    })
+axios.get(`${API_BASE}${genero_EmAlta}`)
+    .then(response => {
+        const filmes = response.data.results.map(filme => {
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        emAlta.innerHTML += filmes
+    })
+axios.get(`${API_BASE}${genero_acao}`)
+    .then(response => {
+        const filmes = response.data.results.map(filme => {
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        acao.innerHTML += filmes
+    })
+axios.get(`${API_BASE}${genero_animacao}`)
+    .then(response => {
+        const filmes = response.data.results.map(filme => {
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        animacao.innerHTML += filmes
+    })
+axios.get(`${API_BASE}${genero_terror}`)
+    .then(response => {
+        const filmes = response.data.results.map(filme => {
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        terror.innerHTML += filmes
+    })
+axios.get(`${API_BASE}${genero_romance}`)
+    .then(response => {
+        const filmes = response.data.results.map(filme => {
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        romance.innerHTML += filmes
+    })
+axios.get(`${API_BASE}${genero_documentario}`)
+    .then(response => {
+        const filmes = response.data.results.map(filme => {
+            return `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" />`
+        }).join(' ');
+        documentario.innerHTML += filmes
+    })
+
+
+
+
+
+let scrollOriginals = 0;
+const LeftOriginals = (div) => {
+
+    scrollOriginals -= 600;
+    div.style.marginLeft = `${-scrollOriginals}px`;
+
+    if (scrollOriginals < 0) {
+        scrollOriginals = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightOriginals = (div) => {
+
+    scrollOriginals += 600;
+    div.style.marginLeft = `${-scrollOriginals}px`;
+
+    if (scrollOriginals > 1950) {
+        scrollOriginals = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+let scrollRecomendados = 0;
+const LeftRecomendado = (div) => {
+
+    scrollRecomendados -= 600;
+    div.style.marginLeft = `${-scrollRecomendados}px`;
+
+    if (scrollRecomendados < 0) {
+        scrollRecomendados = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightRecomendado = (div) => {
+
+    scrollRecomendados += 600;
+    div.style.marginLeft = `${-scrollRecomendados}px`;
+
+    if (scrollRecomendados > 1950) {
+        scrollRecomendados = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+
+let scrollEmAlta = 0;
+const LeftEmAlta = (div) => {
+
+    scrollEmAlta -= 600;
+    div.style.marginLeft = `${-scrollEmAlta}px`;
+
+    if (scrollEmAlta < 0) {
+        scrollEmAlta = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightEmAlta = (div) => {
+
+    scrollEmAlta += 600;
+    div.style.marginLeft = `${-scrollEmAlta}px`;
+
+    if (scrollEmAlta > 1950) {
+        scrollEmAlta = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+
+let scrollAcao = 0;
+const LeftAcao = (div) => {
+
+    scrollAcao -= 600;
+    div.style.marginLeft = `${-scrollAcao}px`;
+
+    if (scrollAcao < 0) {
+        scrollAcao = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightAcao = (div) => {
+
+    scrollAcao += 600;
+    div.style.marginLeft = `${-scrollAcao}px`;
+
+    if (scrollAcao > 1950) {
+        scrollAcao = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+
+let scrollAnimacao = 0;
+const LeftAnimacao = (div) => {
+
+    scrollAnimacao -= 600;
+    div.style.marginLeft = `${-scrollAnimacao}px`;
+
+    if (scrollAnimacao < 0) {
+        scrollAnimacao = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightAnimacao = (div) => {
+
+    scrollAnimacao += 600;
+    div.style.marginLeft = `${-scrollAnimacao}px`;
+
+    if (scrollAnimacao > 1950) {
+        scrollAnimacao = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+
+let scrollTerror = 0;
+const LeftTerror = (div) => {
+
+    scrollTerror -= 600;
+    div.style.marginLeft = `${-scrollTerror}px`;
+
+    if (scrollTerror < 0) {
+        scrollTerror = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightTerror = (div) => {
+
+    scrollTerror += 600;
+    div.style.marginLeft = `${-scrollTerror}px`;
+
+    if (scrollTerror > 1950) {
+        scrollTerror = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+
+let scrollRomance = 0;
+const LeftRomance = (div) => {
+
+    scrollRomance -= 600;
+    div.style.marginLeft = `${-scrollRomance}px`;
+
+    if (scrollRomance < 0) {
+        scrollRomance = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightRomance = (div) => {
+
+    scrollRomance += 600;
+    div.style.marginLeft = `${-scrollRomance}px`;
+
+    if (scrollRomance > 1950) {
+        scrollRomance = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+let scrollDocumentario = 0;
+const LeftDocumentario = (div) => {
+
+    scrollDocumentario -= 600;
+    div.style.marginLeft = `${-scrollDocumentario}px`;
+
+    if (scrollDocumentario < 0) {
+        scrollDocumentario = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+const RightDocumentario = (div) => {
+
+    scrollDocumentario += 600;
+    div.style.marginLeft = `${-scrollDocumentario}px`;
+
+    if (scrollDocumentario > 1950) {
+        scrollDocumentario = 0;
+        div.style.marginLeft = '0px'
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
